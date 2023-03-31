fd019e1fa5b6efaa668e13bca3a8d47ef8f76499
colecrum26 <cwcrumrine@gmail.com>
2023-03-31 12:18:42 -0500

    cleaned up App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, NavLink, Routes, Route} from "react-router-dom";
 import { HomePage, FilmsPage } from "./pages";
 
 function App(props) {
-  const [list, setList] = useState(["ready", "set", "GO"]);
-  const [text, setText] = useState("");
-  let onSubmit = (event) => {
-    event.preventDefault();
-    let newList = [...list, text];
-    setList(newList);
-    setText("");
-  };
 
   return (
     <Router>
@@ -22,28 +14,6 @@ function App(props) {
         <li><NavLink to="/">HOME</NavLink></li>
         <li><NavLink to="/films">FILMS</NavLink></li>
       </nav>
-    <div>
-      <h1>Hello World</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="text"
-          id="text"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <button type="submit" text="add">
-          Submit
-        </button>
-      </form>
-      <ul>
-          {list.map((item, index) => {
-            return <li key={index}> {item} </li>;
-          })}
-      </ul>
-    </div>
     </Router>
   );
 }
